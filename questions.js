3de20914d92b78ded58f59463f977dcbcde3d690
xeno <46341046+nevadaa@users.noreply.github.com>
2023-09-21 20:08:53 -0300

    questions.

--- questions.js
+++ questions.js
@@ -1,13 +1,12 @@
 const questions = { // eslint-disable-line no-unused-vars
-  // revolution vs reform
+  // spiritual vs secular
   question_0: {
-    a: -5,
-    b: 0,
-    c: 0,
-    d: 0,
+    a: 10,
+    b: 2,
+    c: -1,
+    d: 5,
     e: 0,
-    f: 0,
-    g: 0
+
   },
   question_1: {
     a: 10,
@@ -15,639 +14,237 @@ const questions = { // eslint-disable-line no-unused-vars
     c: 0,
     d: 0,
     e: 0,
-    f: 0,
-    g: 0
+
   },
   question_2: {
-    a: -10,
-    b: 0,
+    a: -20,
+    b: -5,
     c: 0,
     d: 0,
     e: 0,
-    f: 0,
-    g: 0
+
   },
   question_3: {
-    a: -10,
-    b: 0,
+    a: 10,
+    b: 2,
     c: 0,
-    d: 0,
+    d: 2,
     e: 0,
-    f: 0,
-    g: 0
+
   },
   question_4: {
-    a: 5,
-    b: 0,
+    a: -20,
+    b: -5,
     c: 0,
     d: 0,
     e: 0,
-    f: 0,
-    g: 0
+
   },
+  // ideal vs morphological
   question_5: {
-    a: -10,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_6: {
-    a: 5,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_7: {
-    a: -5,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_8: {
-    a: 5,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_9: {
-    a: 5,
-    b: 0,
-    c: 0,
-    d: 5,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_10: {
-    a: 5,
-    b: 5,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_11: {
-    a: 0,
+    a: 2,
     b: 10,
     c: 0,
     d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    e: 5,
+
   },
-  question_12: {
+  question_6: {
     a: 0,
-    b: 5,
+    b: 7,
     c: 0,
     d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    e: 4,
+
+  },
+  question_7: {
+    a: -1,
+    b: -20,
+    c: 0,
+    d: 3,
+    e: -5,
+
+  },
+  question_8: {
+    a: 5,
+    b: 8,
+    c: 0,
+    d: 0,
+    e: 10,
+
+  },
+  question_9: {
+    a: 0,
+    b: 15,
+    c: 0,
+    d: 5,
+    e: 12,
+
+  },
+
+  question_10: {
+    a: -10,
+    b: -10,
+    c: 20,
+    d: -5,
+    e: -4,
+
+  },
+  //inhuman vs alterhuman
+  question_11: {
+    a: 2,
+    b: -5,
+    c: 15,
+    d: 2,
+    e: 5,
+
+  },
+  question_12: {
+    a: -5,
+    b: 5,
+    c: 15,
+    d: -10,
+    e: 5,
+
   },
   question_13: {
     a: 0,
     b: 5,
-    c: 0,
+    c: -15  ,
     d: 0,
     e: 0,
-    f: 0,
-    g: 0
+
   },
   question_14: {
-    a: -5,
+    a: -1,
     b: -10,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    c: -15,
+    d: -1,
+    e: -3,
+
   },
+  //rewilding vs synthesis
   question_15: {
-    a: 0,
-    b: 5,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    a: 10,
+    b: 2,
+    c: -2,
+    d: 20,
+    e: -20,
+
   },
   question_16: {
-    a: 0,
-    b: 10,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    a: 5,
+    b: 0,
+    c: 1,
+    d: 15,
+    e: -15,
+
   },
   question_17: {
-    a: 0,
-    b: 5,
+    a: -5,
+    b: -5,
     c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    d: -10,
+    e: 20,
+
   },
   question_18: {
     a: 0,
-    b: -10,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    b: 10,
+    c: 10,
+    d: 10,
+    e: -20,
+
   },
   question_19: {
-    a: 0,
-    b: -10,
+    a: 10,
+    b: 5,
     c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    d: 15,
+    e: -10,
+
   },
+  //assimilation vs liberation
   question_20: {
-    a: 0,
-    b: 0,
-    c: -10,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    a: 1,
+    b: -5,
+    c: 10,
+    d: 5,
+    e: -10,
+
   },
   question_21: {
     a: 0,
     b: 0,
-    c: -5,
+    c: 10,
     d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    e: -15,
+
   },
   question_22: {
     a: 0,
     b: 0,
     c: 5,
     d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    e: -10,
+
   },
   question_23: {
     a: 0,
     b: 0,
     c: 10,
     d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    e: -10,
+
   },
   question_24: {
     a: 0,
     b: 0,
-    c: -5,
+    c: 5,
     d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    e: -10,
+
   },
   question_25: {
     a: 0,
-    b: 0,
-    c: -5,
+    b: 10,
+    c: -4,
     d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    e: 10,
+
   },
   question_26: {
-    a: 0,
-    b: 0,
+    a: 12,
+    b: 5,
     c: -5,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    d: 15,
+    e: -15,
+
   },
   question_27: {
-    a: 0,
-    b: 0,
-    c: -10,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
+    a: -10,
+    b: -5,
+    c: 5,
+    d: 5,
+    e: -5,
+
   },
   question_28: {
-    a: 0,
+    a: 14,
     b: 5,
-    c: 5,
+    c: 0,
     d: 0,
     e: 0,
-    f: 0,
-    g: 0
+
   },
   question_29: {
     a: 0,
     b: 0,
-    c: 10,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_30: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 5,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_31: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 10,
-    e: 0,
-    f: 0,
-    g: -5
-  },
-  question_32: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 5,
-    e: 5,
-    f: 0,
-    g: 0
-  },
-  question_33: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: -5,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_34: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 5,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_35: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 10,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_36: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 5,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_37: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 10,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_38: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 5,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_39: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 5,
-    e: 0,
-    f: 0,
-    g: 0
-  },
-  question_40: {
-    a: -5,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 5,
-    f: 0,
-    g: 0
-  },
-  question_41: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: -5,
-    f: 0,
-    g: 0
-  },
-  question_42: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: -10,
-    f: 0,
-    g: 0
-  },
-  question_43: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: -5,
-    f: 0,
-    g: 0
-  },
-  question_44: {
-    a: 0,
-    b: 0,
-    c: 0,
+    c: 1,
     d: 0,
     e: 10,
-    f: 0,
-    g: 0
+
   },
-  question_45: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: -5,
-    f: 0,
-    g: 0
-  },
-  question_46: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: -10,
-    f: 0,
-    g: 0
-  },
-  question_47: {
-    a: 0,
-    b: 0,
-    c: -5,
-    d: 0,
-    e: -5,
-    f: 0,
-    g: 0
-  },
-  question_48: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 5,
-    f: 0,
-    g: 0
-  },
-  question_49: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: -5,
-    g: 0
-  },
-  question_50: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 10,
-    g: 0
-  },
-  question_51: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 5,
-    g: 0
-  },
-  question_52: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: -5,
-    g: 0
-  },
-  question_53: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: -10,
-    g: 0
-  },
-  question_54: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: -5,
-    g: -5
-  },
-  question_55: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: -5,
-    g: -5
-  },
-  question_56: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: -10,
-    g: 0
-  },
-  question_57: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: -10,
-    g: 0
-  },
-  question_58: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: -5,
-    g: 0
-  },
-  question_59: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 5,
-    g: 0
-  },
-  question_60: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: -10,
-    g: 0
-  },
-  question_61: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: -5,
-    g: 0
-  },
-  question_62: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: -10
-  },
-  question_63: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: -5
-  },
-  question_64: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: -5
-  },
-  question_65: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 10
-  },
-  question_66: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 5
-  },
-  question_67: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: -5
-  },
-  question_68: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 10
-  },
-  question_69: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: -5
-  },
-  question_70: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 5
-  },
-  question_71: {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: -10
-  }
 }
 
 Object.size = function (obj) {
